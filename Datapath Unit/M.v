b2ddb9e0c393d2d79e16491e03163878575e9846
LimKienLiang <87468443+LimKienLiang@users.noreply.github.com>
2021-07-17 13:01:28 +0800

    Update M.v

--- Datapath Unit/M.v
+++ Datapath Unit/M.v
@@ -1,7 +1,7 @@
 module M (
-  input [3:0] Min;
-  input clk, load, rst,
-  output reg [3:0] Mout
+  input [3:0]Min;
+  input clk,load,rst,
+  output reg [3:0]Mout
   );
   always @ (posedge clk, posedge rst)
   if (rst) begin Mout <= 0; end
