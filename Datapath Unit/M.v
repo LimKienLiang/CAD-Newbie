d0066bdae1ea338cbbeacf4bcddd32d83ce0f109
LimKienLiang <87468443+LimKienLiang@users.noreply.github.com>
2021-07-17 12:43:49 +0800

    Update and rename M Register to M.v

---
+++ Datapath Unit/M.v
@@ -0,0 +1,9 @@
+module M (
+  input [3:0] Min;
+  input clk, load, rst,
+  output reg [3:0] Mout
+  );
+  always @ (posedge clk, posedge rst)
+  if (rst) begin Mout <= 0; end
+  else begin if (load) Mout <= Min; end
+endmodule
